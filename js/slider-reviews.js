cdad3c27d02a4ba174cdb4573df4d8c2d9fa36c9
Vasily Lopatkin <81714297+LiViS-code@users.noreply.github.com>
2023-10-01 09:17:30 +0300

    optimisation script and css

--- js/slider-reviews.js
+++ js/slider-reviews.js
@@ -1,43 +1,52 @@
-let offset = 0;
-let currSliderNumn = 1;
-const sliderLine = document.querySelector(".slider-line");
-const widthNumber = document.querySelector(".review-cards-wrap").offsetWidth;
-const slider = document.querySelector(".slider");
-const sliderAll = document.querySelectorAll(".slider");
-const btnNext = document.querySelector(".btn-next");
-const btnPrev = document.querySelector(".btn-prev");
-const sliderQuantity = sliderAll.length;
+let offset = 0; // величина сменщения слайдов
+let currSliderNumn = 1; // номер отображаемого слайдера в текущий момент
+const sliderLine = document.querySelector(".slider-line"); // полоса слайдов
+const widthNumber = document.querySelector(".review-cards-wrap").offsetWidth; // ширина кадра просмотра на странице
+const slider = document.querySelector(".slider"); // один слайд
+const sliderAll = document.querySelectorAll(".slider"); // все слайды из разметки
+const btnNext = document.querySelector(".btn-next"); // кнопка следующий слайд
+const btnPrev = document.querySelector(".btn-prev"); // кнопка предыдущий слайд
+const sliderQuantity = sliderAll.length; // количество слайдов
+const styleSliderLine = getComputedStyle(sliderLine);
+const gapSlider = parseInt(styleSliderLine.gap); // расстояние между слайдами в полосе слайдов из CSS
 
 function init() {
   sliderAll.forEach((item) => {
     item.style.width = widthNumber + "px";
-  });
+  }); // утсановить ширину слайдера равной ширине кадра просмотра
 }
 
-init();
-
-btnNext.addEventListener("click", () => {
-  offset += widthNumber;
-  currSliderNumn += 1;
+function nextSlider() {
+  offset += widthNumber; // приращение смещения на ширину слайдера
+  currSliderNumn++; // приращение номера текущего слайдера
 
   if (currSliderNumn > sliderQuantity) {
     offset = 0;
     currSliderNumn = 1;
   }
 
-  document.getElementById("current").textContent = currSliderNumn;
-  sliderLine.style.left = -offset + "px";
-});
+  moveSlider(offset, currSliderNumn); // переместить слайдер
+}
 
-btnPrev.addEventListener("click", () => {
+function prevSlider() {
   offset -= widthNumber;
-  currSliderNumn -= 1;
+  currSliderNumn--;
 
   if (currSliderNumn < 1) {
     offset = widthNumber * (sliderQuantity - 1);
     currSliderNumn = sliderQuantity;
   }
 
-  document.getElementById("current").textContent = currSliderNumn;
-  sliderLine.style.left = -offset + "px";
-});
+  moveSlider(offset, currSliderNumn);
+}
+
+function moveSlider(offset, currSliderNumn) {
+  document.getElementById("current").textContent = currSliderNumn; // вывести новое значение текущего слайдера
+  sliderLine.style.left = -offset - gapSlider * (currSliderNumn - 1) + "px"; // сместить слайдер
+}
+
+init();
+
+btnNext.addEventListener("click", nextSlider);
+
+btnPrev.addEventListener("click", prevSlider);
